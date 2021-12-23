3ca1e0a896579fb89a6540fe6a27562b20fc5018
commit <3294095282@qq.com>
2021-12-23 18:34:18 +0800

    my first commit

---
+++ Student.java
@@ -0,0 +1,10 @@
+public class Student {
+    int id;
+    String name;
+    String age;
+ 
+     @Override
+        public String toString() {
+        return "student [id=" + id + ", name=" + name + ", age=" + age + "]";
+    }
+}
